66dc3fd2288f8b1156f336794950bbe3fdc5ea37
rumeysa1017 <rumeysa1017@gmail.com>
2025-11-04 21:42:31 +0100

    Add files via upload

--- CRM/mentor_meeting_page.py
+++ CRM/mentor_meeting_page.py
@@ -13,7 +13,6 @@ class MentorMeetingWindow(BaseWindow):
         loadUi(os.path.join(UI_DIR, "Mentor_Meeting_Page.ui"), self)
         self.setWindowTitle("Mentor Meeting Page")
         self.setFixedSize(1000, 600)
-        self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
         self.move_to_last_position()
 
         self.pushButton_Return_REFERENCE_menu.clicked.connect(self.return_to_menu)
